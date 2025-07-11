f6aace483385354efe1f321cdc29b3adfaa97ca8
dra18 <ramirezandresdaniela@gmail.com>
2025-07-10 21:14:47 -0600

    add scene SaveMenu

---
+++ Assets/Project/Scripts/LoadGameUIManager.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadGameUIManager : MonoBehaviour
+{
+    public Transform contentPanel;
+    public GameObject slotPrefab;
+
+    // Simulación de partidas guardadas
+    private List<string> partidasSimuladas = new List<string> {
+        "Partida 1", "Partida 2", "Partida 3"
+    };
+
+    void Start()
+    {
+        MostrarPartidasSimuladas();
+    }
+
+    void MostrarPartidasSimuladas()
+    {
+        foreach (Transform hijo in contentPanel)
+        {
+            Destroy(hijo.gameObject); // Limpia anteriores
+        }
+
+        foreach (string nombre in partidasSimuladas)
+        {
+            GameObject slot = Instantiate(slotPrefab, contentPanel);
+            slot.transform.Find("SlotGameText").GetComponent<TMP_Text>().text = nombre;
+
+            Button btnCargar = slot.transform.Find("LoadButton").GetComponent<Button>();
+            Button btnEliminar = slot.transform.Find("DeleteButton").GetComponent<Button>();
+
+            btnCargar.onClick.AddListener(() => {
+                Debug.Log("Cargar partida: " + nombre);
+            });
+
+            btnEliminar.onClick.AddListener(() => {
+                Debug.Log("Eliminar partida: " + nombre);
+                partidasSimuladas.Remove(nombre);
+                MostrarPartidasSimuladas();
+            });
+        }
+    }
+}
